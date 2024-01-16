744310142cdc79fdd312c2eab9116c7d451ed2f1
Ujjwal <Ujjwal22154010@akgec.ac.in>
2024-01-16 11:27:46 +0530

    error resolved

--- app/src/main/java/com/android/messenger/Message.kt
+++ app/src/main/java/com/android/messenger/Message.kt
@@ -4,6 +4,8 @@ class Message {
     var message:String? = null
     var senderId:String? = null
 
+    constructor()
+
     constructor(message:String?,senderId:String?){
         this.message = message
         this.senderId = senderId
